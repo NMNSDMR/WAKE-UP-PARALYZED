a9ccc3c55f189641215c14be994043295398d5db
cerf <148485154+NMNSDMR@users.noreply.github.com>
2024-02-07 13:43:31 +0300

    Update ping_pong.py

--- Main Project/games/ping_pong.py
+++ Main Project/games/ping_pong.py
@@ -11,7 +11,6 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ping Pong")
 
 # Цвета
-WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [(255, 0, 0), (255, 165, 0), (255, 255, 0), (0, 128, 0), (0, 0, 255), (75, 0, 130), (238, 130, 238)]
 
@@ -36,8 +35,8 @@ font_vel = [FONT_SPEED, FONT_SPEED]
 # Функция отрисовки платформ и надписи
 def draw(canvas):
     canvas.fill(BLACK)
-    pygame.draw.rect(canvas, WHITE, (player1_pos[0], player1_pos[1], PAD_WIDTH, PAD_HEIGHT))
-    pygame.draw.rect(canvas, WHITE, (player2_pos[0], player2_pos[1], PAD_WIDTH, PAD_HEIGHT))
+    pygame.draw.rect(canvas, (255, 255, 255), (player1_pos[0], player1_pos[1], PAD_WIDTH, PAD_HEIGHT))
+    pygame.draw.rect(canvas, (255, 255, 255), (player2_pos[0], player2_pos[1], PAD_WIDTH, PAD_HEIGHT))
     font_surface = font.render("DVD", True, random.choice(COLORS))
     canvas.blit(font_surface, font_pos)
 
@@ -72,21 +71,4 @@ while True:
         player1_pos[1] += PAD_SPEED
     if keys[pygame.K_UP]:
         player2_pos[1] -= PAD_SPEED
-    if keys[pygame.K_DOWN]:
-        player2_pos[1] += PAD_SPEED
-
-    # Ограничения для платформ
-    if player1_pos[1] <= 0:
-        player1_pos[1] = 0
-    elif player1_pos[1] >= HEIGHT - PAD_HEIGHT:
-        player1_pos[1] = HEIGHT - PAD_HEIGHT
-    if player2_pos[1] <= 0:
-        player2_pos[1] = 0
-    elif player2_pos[1] >= HEIGHT - PAD_HEIGHT:
-        player2_pos[1] = HEIGHT - PAD_HEIGHT
-
-    update()
-
-    draw(WINDOW)
-    pygame.display.update()
-    clock.tick(60)
+    if keys[pygame⬤
